130b819c7531c3ca07c208a9d1ad9d08369ef4c2
anvar <khalitoff@live/ru>
2020-09-27 22:49:03 +0500

    Задача 6 - решена

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -7,9 +7,55 @@ namespace lesson2
     {
         static void Main(string[] args)
         {
+            string login, password;
+            double height, weight, bmi, exceedWeight;
+            int tryCount = 0;
+            bool authorized;
+
             //Console.WriteLine(Min(100,200,300));
             //Console.WriteLine(DigitCounter(-123));
-            SumOfOddNum();
+            //SumOfOddNum();
+            //Задача 4
+            //Console.WriteLine("Программа авторизации пользователя.");
+            //do
+            //{
+            //    Console.Write("Введите логин: ");
+            //    login = Console.ReadLine();
+            //    Console.Write("Введите пароль: ");
+            //    password = Console.ReadLine();
+            //    Authorization(login, password, out authorized);
+            //    if (authorized) break;
+            //    else tryCount++;
+
+            //} while (tryCount <3);
+
+            //if (authorized) Console.WriteLine("Вы авторизованы!");
+            //else Console.WriteLine("Попытки входа исчерпаны!");
+
+            // Задача 5 - ИМТ
+            //Console.Write("Ваш рост (м): ");
+            //height = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Ваш вес (кг): ");
+            //weight = Convert.ToDouble(Console.ReadLine());
+
+            //bmi = BodyMassIndex(height, weight, out exceedWeight);
+
+            //Console.WriteLine($"Ваш ИМТ = {bmi:f2}. ");
+            //if (bmi < 25.0) Console.Write($"Вам следует набрать {exceedWeight:f2} кг");
+            //else if (bmi > 30.0) Console.Write($"Вам следует похудеть на {exceedWeight:f2} кг");
+            //else Console.Write($"У вас нормальный вес!");
+
+            // Задача 6 - число хороших чисел
+            DateTime dt1 = DateTime.UtcNow;
+            int GoodNumCount = 0;
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                if (IsGood(i)) GoodNumCount++;
+            }
+            DateTime dt2 = DateTime.UtcNow;
+            Console.WriteLine($"количества хороших чисел: {GoodNumCount}");
+            Console.WriteLine($"Потрачено времени: {dt2.Subtract(dt1)}");
         }
 
         #region Задача 1 - найти минимальное число из трёх
@@ -57,6 +103,34 @@ namespace lesson2
         }
 
         #endregion
+
+        #region Задача 4 - авторизация
+        static void Authorization(string login, string password, out bool authorized)
+        {
+            authorized = login.Equals("root") && password.Equals("GeekBrains");
+        }
+        #endregion
+
+        static double BodyMassIndex(double height, double weight, out double excessWeight)
+        {
+            double bmi = weight / height / height;
+            if (bmi > 30.0) excessWeight = weight - 30.0 * height * height;
+            else if (bmi < 25.0) excessWeight = 25.0 * height * height - weight;
+            else excessWeight = 0.0;
+            return bmi;
+        }
+
+        static bool IsGood(int number)
+        {
+            int sumOfDigit = 0;
+            string s = Convert.ToString(number);
+            for (int i = 0; i < s.Length; i++)
+            {
+                sumOfDigit += Convert.ToInt32(s[i]);
+            }
+
+            return (number % sumOfDigit == 0);
+        }
     }
 
 
